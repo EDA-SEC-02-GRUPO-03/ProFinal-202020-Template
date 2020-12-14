019b2d0223b3e56dc04fffb91b095abd70205c58
FeWiesner <f.wiesner@uniandes.edu.co>
2020-12-13 23:39:02 -0500

    doc

--- App/controller.py
+++ App/controller.py
@@ -61,8 +61,6 @@ def loadTrips(taxi_trips, size):
         if filename.endswith('.csv') and size in filename:
             print('⏳ Cargando archivo: ' + filename)
             loadFile(taxi_trips, filename)
-    print(taxi_trips['horas'])
-
     return taxi_trips
 
 
